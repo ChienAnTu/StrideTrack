207e1bdb6054a14d9287c6f8cf794bcb8b0e8227
Wxvxl <metal123cool1115@gmail.com>
2025-05-08 02:43:41 +0800

    Update Foreign Key in Activity to have a table name.

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
+    __tablename__ = "Users"
     user_id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str] = mapped_column(unique=True) 
@@ -19,7 +20,8 @@ class User(db.Model):
 
 
 class ActivityRegistry(db.Model):
-    user_id: Mapped[int] = mapped_column(ForeignKey(User.user_id), primary_key=True)
+    __tablename__ = "ActivityRegistry "
+    user_id: Mapped[int] = mapped_column(ForeignKey(User.user_id), primary_key=True, name="upload_user_id")
     upload_time: Mapped[datetime] = mapped_column(DateTime, primary_key=True)
     activity_date: Mapped[date] = mapped_column(Date, nullable=False)
     activity_type: Mapped[str] = mapped_column(nullable=False)
