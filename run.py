8dc907b4939818ab971313571aed4753357a8f7d
Wxvxl <metal123cool1115@gmail.com>
2025-05-07 14:54:57 +0800

    Migrations folder commit

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = create_app()
 def testAddition(user_id, name, email):
     to_add = User(username=name, user_id = user_id, email = email)
     db.session.add(to_add)
-    
+        
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
