16e94142b69171ae57e60ab3165f5720fe02f867
Wxvxl <metal123cool1115@gmail.com>
2025-05-08 19:40:53 +0800

    FIX: Register and Login system

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, g, session
 # Import backend modules below
 from app.config import Config
 from app.database import db, migrate
+from app.models import User
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.orm import DeclarativeBase
@@ -9,6 +10,15 @@ from sqlalchemy.orm import DeclarativeBase
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    # Middleware to set user in global context
+    @app.before_request
+    def load_logged_in_user():
+        user_id = session.get('user_id')
+        if user_id is None:
+            g.user = None
+        else:
+            g.user = User.query.get(user_id)
 
     # ----Attach database to app here----
     db.init_app(app)
@@ -21,6 +31,6 @@ def create_app():
 
     from app import models  # Ensure models are imported
     return app
-    
+
 
 
