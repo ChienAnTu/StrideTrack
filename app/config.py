2f77d18b0ec0aa9681c7f093c5185f7341b7aa68
Wxvxl <metal123cool1115@gmail.com>
2025-05-08 13:21:57 +0800

    FIX: remove print statement

--- app/config.py
+++ app/config.py
@@ -2,8 +2,6 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 default_db_loc = 'sqlite:///' + os.path.join(basedir, 'app.db')  
-print(default_db_loc)
-
 
 class Config:
     SQLALCHEMY_DATABASE_URI = default_db_loc
