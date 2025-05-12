f961fb91471f89ed5fa1f713abf3e085aab504a3
Wxvxl <metal123cool1115@gmail.com>
2025-05-12 17:21:51 +0800

    Create sharedActivity database table for sharing function

--- app/models.py
+++ app/models.py
@@ -5,8 +5,7 @@ from datetime import date, time, datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-
-class User(UserMixin, db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "Users"
     user_id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
@@ -27,7 +26,6 @@ class User(UserMixin, db.Model):
         return self.user_id
 
 
-
 class ActivityRegistry(db.Model):
     __tablename__ = "ActivityRegistry"
     upload_user_id: Mapped[int] = mapped_column(ForeignKey('Users.user_id'), primary_key=True)
@@ -35,7 +33,10 @@ class ActivityRegistry(db.Model):
     activity_date: Mapped[date] = mapped_column(Date, nullable=False)
     activity_type: Mapped[str] = mapped_column(nullable=False)
     activity_length: Mapped[time] = mapped_column(Time, nullable=False)
-    calories_burned: Mapped[float] = mapped_column(nullable=False, default=0.0)  # ✅ ADD THIS
+    calories_burned: Mapped[float] = mapped_column(nullable=False, default=0.0)
 
-
-    
+class SharedActivity(db.Model):
+    __tablename__ = "SharedActivity"
+    sharing_user: Mapped[int] = mapped_column(ForeignKey('ActivityRegistry.upload_user_id'), primary_key=True)
+    activity_upload_time: Mapped[datetime] = mapped_column(ForeignKey('ActivityRegistry.upload_time'), primary_key=True)
+    user_shared_with: Mapped[int] = mapped_column(ForeignKey('Users.user_id'), primary_key=True)
