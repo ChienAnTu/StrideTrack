2b0c071c2f3245d574d90ec5b25e895265415b42
unknown <24245807@student.uwa.edu.au>
2025-05-13 13:52:14 +0800

    added challenges. user can now select pre-tailored trails

--- app/static/script/script.js
+++ app/static/script/script.js
@@ -24,3 +24,22 @@ function openSignUp() {
       closeSignup();
     }
   });
+
+  // Define a trail as an array of LatLng points
+var trailCoordinates = [
+  [51.505, -0.09],
+  [51.506, -0.092],
+  [51.507, -0.093],
+  [51.508, -0.095]
+];
+
+// Add the polyline to the map
+var trailPath = L.polyline(trailCoordinates, {
+  color: 'blue',
+  weight: 5,
+  opacity: 0.7,
+  smoothFactor: 1
+}).addTo(map);
+
+// Zoom the map to fit the trail
+map.fitBounds(trailPath.getBounds());
