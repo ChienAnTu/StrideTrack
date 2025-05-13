99245c75b2dbb92705049ef3f5753de8956f4790
ChienAnTu <24367005@student.uwa.edu.au>
2025-05-13 20:24:55 +0800

    deleted or commented useless comments

--- app/routes.py
+++ app/routes.py
@@ -3,11 +3,10 @@ from flask import request, redirect, url_for, session, flash
 from app.models import User, db, ActivityRegistry, SharedActivity
 from datetime import datetime, timedelta
 from flask_login import login_user, logout_user, login_required, current_user
-from io import TextIOWrapper
+# from io import TextIOWrapper
 import io
 import csv
 
-# from app import db  # Uncomment if using database
 
 def register_routes(app):
     @app.route('/')
--- app/static/script/script.js
+++ app/static/script/script.js
@@ -1,30 +1,3 @@
-// function openSignIn() {
-//     document.getElementById('loginModal').classList.remove('hidden');
-//   }
-  
-//   function closeSignIn() {
-//     document.getElementById('loginModal').classList.add('hidden');
-//   }
-
-//   // Open modal
-// function openSignUp() {
-//     document.getElementById('signupModal').classList.remove('hidden');
-//     document.body.classList.add('overflow-hidden'); // Prevent scrolling
-//   }
-  
-//   // Close modal
-//   function closeSignup() {
-//     document.getElementById('signupModal').classList.add('hidden');
-//     document.body.classList.remove('overflow-hidden');
-//   }
-  
-//   // Close when clicking outside modal
-//   document.getElementById('signupModal').addEventListener('click', (e) => {
-//     if (e.target === document.getElementById('signupModal')) {
-//       closeSignup();
-//     }
-//   });
-
 // Open login modal
 function openSignIn() {
   const loginModal = document.getElementById('loginModal');
