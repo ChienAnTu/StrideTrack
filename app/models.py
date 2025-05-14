56bb6fc5b0fdf26651165ea263758466ad74bf75
Wxvxl <metal123cool1115@gmail.com>
2025-05-15 01:52:15 +0800

    Activity Length refactored to use int as length is counted in minutes

--- app/models.py
+++ app/models.py
@@ -27,7 +27,7 @@ class ActivityRegistry(db.Model):
     upload_time: Mapped[datetime] = mapped_column(DateTime, primary_key=True)
     activity_date: Mapped[date] = mapped_column(Date, nullable=False)
     activity_type: Mapped[str] = mapped_column(nullable=False)
-    activity_length: Mapped[time] = mapped_column(Time, nullable=False)
+    activity_length: Mapped[int] = mapped_column(Integer, nullable=False)
     calories_burned: Mapped[float] = mapped_column(nullable=False, default=0.0)
 
 class SharedActivity(db.Model):
